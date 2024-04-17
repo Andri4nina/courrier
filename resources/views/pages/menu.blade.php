a0db2a46d26936f176099efd1813618e8eb4d06f
Raherinotoavina <raherinotoavinasa@gmail.com>
2024-04-17 23:00:42 +0300

    Add a header to the menu

--- resources/views/pages/menu.blade.php
+++ resources/views/pages/menu.blade.php
@@ -5,7 +5,8 @@
 
 @section("content")
 
-<div class="w-full max-h-screen h-screen relative flex justify-center items-center">
+{{ view('pages.layouts.headers') }}
+<div class="w-full max-h-screen h-screen relative flex justify-center">
     <div class=" gap-12 flex justify-center items-start">
         <div>
             <div class="w-full grid grid-cols-2 gap-2 mb-2 dashboard">
@@ -104,29 +105,6 @@
                 </div>
             </div>
         </div>
-
-    </div>
-
-    <div class="absolute top-0 left-0 w-full h-auto">
-        <div class="flex justify-between px-16 pt-5 items-center">
-            <div class=" h-16 w-16  Logo">
-                <a href="#">
-                    <div class="absolute top-5 left-5 flex justify-center gap-5 items-center">
-                        <div class=" h-16 w-16  border rotate-45  Logo ">
-                            <img src="{{ asset('images/Postman.png')}}" alt="LogoPosteman" class="-rotate-45 w-full h-full object-fill">
-                        </div>
-                        <div class="text-white text-3xl font-semibold">
-                            PosteMan
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="text-white flex justify-center items-center gap-2">
-               <span class="font-semibold">Andrianina</span> <i class="text-xl bx bx-user"></i>
-            </div>
-        </div>
-
-
 
     </div>
 
