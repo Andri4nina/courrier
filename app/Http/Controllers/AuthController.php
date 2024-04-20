c1363e85cc034a3daae7927e05e07feb45a41b54
Raherinotoavina <raherinotoavinasa@gmail.com>
2024-04-20 10:00:42 +0300

    Get all region

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\LoginRequest;
+use App\Models\Poste;
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
 
 class AuthController extends Controller
 {
       // Affiche le formulaire de connexion
-      public function login()
-      {
-          return view('login.login');
-      }
+        public function login()
+        {  
+            $postes = Poste::all();
+
+            return view('login.login', ["postes" => $postes]);
+        }
        // Effectue le login
        public function dologin()
        {
