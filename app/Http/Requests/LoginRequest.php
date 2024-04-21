fcc8a3ec34d79122cae8cb991443bb01b84b2b00
Andri4nina <raso4m4ndrianina@gmail.com>
2024-04-21 11:45:44 +0300

    ts aiko fa mis erreur tampoka le ato fa ela ela be

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -22,6 +22,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'name'=>'required',
             'email'=>'required|email',
             'password'=>'required'
         ];
